88aa755242dac093433341a0c14416f3150a751b
Solid-Samurai <tamilhacks26@gmail.com>
2025-06-15 19:32:54 +0530

    Enable CORS for frontend access

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS  
 from app.routes import main
 from dotenv import load_dotenv
 
@@ -6,5 +7,6 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)  
     app.register_blueprint(main)
     return app
